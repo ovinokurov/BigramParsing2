7b4b490ec81de34462cdfef19bc6ed5b5c912266
olegv <olegvinokurov@hotmail.com>
2018-07-06 22:18:39 -0700

    added interface

--- ConsoleBigramParsing2/Program.cs
+++ ConsoleBigramParsing2/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text.RegularExpressions;
+
 
 namespace ConsoleBigramParsing2
 {
@@ -36,47 +36,5 @@ namespace ConsoleBigramParsing2
             Console.ReadLine();
         }
     }
-    public class HistogramService
-    {
-        public Dictionary<string, int> ProcessFile(string path)
-        {
-            var dictionaryHistogram = new Dictionary<string, int>();
-
-            if (!File.Exists(path)) return dictionaryHistogram;
-
-            using (var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (var buffered = new BufferedStream(stream))
-            using (var reader = new StreamReader(buffered))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var words = RemoveNonLetters(line).ToLower().Split(' ');
-
-                    for (var i = 0; i < words.Length - 1; i++)
-                    {
-                        var phrase = $"{words[i]} {words[i + 1]}";
-
-                        var ext = dictionaryHistogram.FirstOrDefault(r => r.Key == phrase);
-
-                        if (ext.Value > 0)
-                        {
-                            dictionaryHistogram[phrase] = ext.Value + 1;
-                        }
-                        else
-                        {
-                            dictionaryHistogram.Add(phrase, 1);
-                        }
-                    }
-                }
-            }
-
-            return dictionaryHistogram;
-        }
-
-        private static string RemoveNonLetters(string input)
-        {
-            return Regex.Replace(input, "[^A-Za-z0-9 ]", "");
-        }
-    }
+    
 }
